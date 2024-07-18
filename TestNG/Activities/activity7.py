af7ad0e13da3e09c0ab3a81a96031863c1c9254b
Manisha Khatri <manisha.khatri1@ibm.com>
2024-07-18 13:15:55 +0530

    added remaining files

--- TestNG/Activities/activity7.py
+++
@@ -1,8 +0,0 @@
-numbers = input("Enter elements of the list separated by space: ")
-list = numbers.split()
-list = [int(num) for num in list]
-print("Elements in list",list)
-total=0
-for ele in range(0, len(list)):
-    total += list[ele]
-print("Sum of elements in list",total)
